2483d5edaea9d9d6fbbaee7a37857bee214e03bd
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-11-27 12:21:00 +0000

    fix

--- src/main.sol
+++ src/main.sol
@@ -1,9 +1,7 @@
 
 contract NoirTest {
 
-    /// A message that can be called on instantiated contracts.
-    /// This one flips the value of the stored `bool` from `true`
-    /// to `false` and vice versa.
+    // Demo function, returns the sum of two numbers
     function main(uint256 x, uint256 y) public pure returns (uint256) {
         return x + y;
     }
